0001bbfb33f1f772cc319c33f3742bfdf504b0fd
chun <yanchun.chang5@gmail.com>
2025-07-26 00:17:44 +0200

    added new components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CaptionComponent } from './components/caption/caption.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { SimplificationComponent } from './components/simplification/simplification.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'homepage', pathMatch: 'full' },
     { path: 'homepage', component: HomepageComponent },
     { path: 'caption', component: CaptionComponent },
+    { path: 'simplification', component: SimplificationComponent },
 ];
